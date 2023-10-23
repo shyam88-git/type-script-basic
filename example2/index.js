e39c503517bada29c9a23eb51ba03de52a506fb0
Shyam Sharki <ssk66221@gmail.com>
2023-10-23 10:11:18 +0545

    array object

---
+++ example2/index.js
@@ -0,0 +1,6 @@
+//Array object in typescript
+var arr_names = new Array(4);
+for (var i = 0; i < arr_names.length; i++) {
+    arr_names[i] = i * 2;
+    console.log(arr_names[i]);
+}
---
+++ example2/index.ts
@@ -0,0 +1,10 @@
+//Array object in typescript
+
+
+var arr_names:number[]=new Array(4);
+for(let i=0;i<arr_names.length;i++){
+
+    arr_names[i]=i*2;
+
+    console.log(arr_names[i]);
+}
