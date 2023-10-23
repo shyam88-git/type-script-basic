4aa3335ff29e92db0e7cb285b87f04b2f9d202bb
Shyam Sharki <ssk66221@gmail.com>
2023-10-23 14:31:56 +0545

    map()

---
+++ example10/index.js
@@ -0,0 +1,3 @@
+var arr = [10, 20, 30, 40, 50];
+var newArr = arr.map(function (ele) { return ele * 2; });
+console.log(newArr);
---
+++ example10/index.ts
@@ -0,0 +1,4 @@
+let arr:number[]=[10,20,30,40,50];
+
+let newArr=arr.map((ele)=>ele*2);
+console.log(newArr);
